10fcff90a6118b7657f3ad13dc27253ef913c82a
Paulo Camilo <pcamilo@innovagency.com>
2020-01-30 16:52:27 +0000

    Initial commit

---
+++ app/src/main/java/com/seekbarandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.seekbarandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+import com.seekbar.rangeseekbar.RangeSeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
